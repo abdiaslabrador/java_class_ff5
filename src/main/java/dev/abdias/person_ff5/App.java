45422b3fb4684208ee3e165212db25bc68b332bf
abdias labrador <abdiaslabrador@gmail.com>
2024-10-31 10:35:22 +0100

    feat: Person Class added with the constructor and the showInfo function

--- src/main/java/dev/abdias/person_ff5/App.java
+++ src/main/java/dev/abdias/person_ff5/App.java
@@ -1,10 +1,12 @@
 package dev.abdias.person_ff5;
+import java.util.Date;
 
 public final class App {
     private App() {
     }
 
         public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Person abdias = new Person("Abdías", "Labrador", "Z26185465W",  new Date(1997, 5, 25));
+        abdias.showInfo();
     }
 }
--- src/main/java/dev/abdias/person_ff5/Person.java
+++ src/main/java/dev/abdias/person_ff5/Person.java
@@ -1,6 +1,27 @@
 package dev.abdias.person_ff5;
+import java.util.Date;
 
 public class Person {
-    
+    private String name;
+    private String lastName;
+    private String ci;
+    private Date birthDate;
+
+    public Person(String name, String lastName, String ci, Date birthDate) {
+        this.name = name;
+        this.lastName = lastName;
+        this.ci = ci;
+        this.birthDate = birthDate;
+    }
+
+
+    public void showInfo(){
+        System.out.println("Name: " + this.name);
+        System.out.println("Lastname: " + this.lastName);
+        System.out.println("CI: " + this.ci);
+        System.out.println("Birthdate: " + this.birthDate.getYear() + "-" + (this.birthDate.getMonth()+1) + "-" + this.birthDate.getDay());
+    }
     
 }
+
+
